612b71b51f21e98f86e874c50506ee9672bf228a
Peter Skeide <peterskeide@gmail.com>
2009-06-22 15:49:57 +0200

    Added previous/next week functionality to TimeEntriesController

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -2,32 +2,29 @@ class TimeEntriesController < ApplicationController
   
   before_filter :check_authentication
     
-  def index
-    @user = @current_user
-    find_activities
+  def index    
     @date = Date.today
-    monday = @date.beginning_of_week
-    time_entries = @user.time_entries.between(monday, monday.+(6))
-    @activities = {}
-    if time_entries
-      activity_names = time_entries.collect { |te| te.activity.name }.uniq
-      activity_names.each { |name| @activities[name] = [] }
-      time_entries.sort.each { |te| @activities[te.activity.name]  << te } 
-    end
+    prepare_view(@date)
   end
   
   def previous
+    @date = Date.parse(params[:date]).- 7
+    prepare_view(@date)
+    render :index
   end
   
   def next
+    @date = Date.parse(params[:date]).+ 7
+    prepare_view(@date)
+    render :index
   end
   
   def new
-    activity = Activity.find_by_id(params[:activity][:activity_id])
+    @activity = Activity.find_by_id(params[:activity][:activity_id])
     @user = @current_user
-    @date = Date.today.beginning_of_week
+    @date = Date.parse(params[:date]).beginning_of_week
     @time_entries = []
-    7.times { |i| @time_entries << @user.time_entries.create(:date => @date.+(i), :activity => activity) }
+    7.times { |i| @time_entries << @user.time_entries.create(:date => @date.+(i), :activity => @activity) }
     render :edit
   end
    
@@ -35,11 +32,13 @@ class TimeEntriesController < ApplicationController
     @user = User.find_by_id(params[:user][:id])
     if @user.update_attributes(params[:user])
       flash[:notice] = "Time entries successfully saved"
-      redirect_to time_entries_path
+      @date = Date.parse(params[:date]).beginning_of_week 
+      prepare_view(@date)
+      render :index
     else
-      @date = Date.today.beginning_of_week
-      @time_entries = []
-      7.times { |i| @time_entries << @user.time_entries.build(:date => @date.+(i)) }
+      @date = Date.parse(params[:date]).beginning_of_week
+      activity_id = params[:activity_id].to_i
+      @time_entries = @user.time_entries.for_activity(activity_id).between(@date, @date.+(6))
       render :edit
     end
   end
@@ -54,9 +53,22 @@ class TimeEntriesController < ApplicationController
   def destroy
   end
   
-  private
+  private 
+  
+  def prepare_view(date)
+    find_activities
+    monday = date.beginning_of_week
+    time_entries = @user.time_entries.between(monday, monday.+(6))
+    @activities = {}
+    if time_entries
+      activity_names = time_entries.collect { |te| te.activity.name }.uniq
+      activity_names.each { |name| @activities[name] = [] }
+      time_entries.sort.each { |te| @activities[te.activity.name]  << te } 
+    end
+  end
   
   def find_activities
+    @user = @current_user
     activities = @user.activities + Activity.find_all_by_default_activity(true) 
     @activity_options = activities.collect { |a| [ a.name, a.id ] }
   end
