cbcbfcea73d703cbf9a87c0aca374b73a9db6719
Peter Skeide <peterskeide@gmail.com>
2009-07-06 10:53:11 +0200

    More refactoring of configurable authentication

--- config/initializers/timeflux_config.rb
+++ config/initializers/timeflux_config.rb
@@ -3,6 +3,6 @@ require 'timeflux'
 # Uncomment to authenticate with LDAP.
 #TimeFlux.configure do |config|
 #  config.use_ldap= true
-#  config.ldap_host= 'jokke.conduct.no'
-#  config.ldap_base= 'ou=people,dc=conduct,dc=no'
+#  config.ldap_host= 'your.host.here'
+#  config.ldap_base= 'ou=people,dc=foobar,dc=com'
 #end
