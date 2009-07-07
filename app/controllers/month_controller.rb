e305ffd802f53bd998e56b3e952bca575bf4a151
Håkon Torjus Bommen <hakon.bommen@conduct.no>
2009-07-07 08:32:03 +0200

    added summary to month review

--- app/controllers/month_controller.rb
+++ app/controllers/month_controller.rb
@@ -18,23 +18,37 @@ class MonthController < ApplicationController
     setup_month_view
   end
 
+  def summary
+    setup_month_view
+  end
+
   def listing
-    setup_calender
-    user = current_user_session.user
-      time_data = []
-      data =[]
-      user.time_entries.between(@day,(@day >> 1) -1).sort.each do |t|
-        time_data << [t.activity.name, t.hours, t.date, t.notes] if t.hours > 0
-        data << {"activity_name" =>t.activity.name, "hours" => t.hours, "date" => t.date, "notes" => t.notes} if t.hours > 0
+    setup_month_view
+
+    time_data = []
+    @user.time_entries.between(@day,(@day >> 1) -1).sort.each do |t|
+      time_data << [t.activity.name, t.hours, t.date, t.notes] if t.hours > 0
     end
-    headers = ["activity_name","hours","date","notes"]
 
     table = Ruport::Data::Table.new( :data => time_data,
-          :column_names => headers.collect { |h| h.capitalize.gsub('_',' ') } )
-    table.sort_rows_by!(["date"])
+      :column_names => ["Activity name","Hours","Date","Notes"] )
+    table.sort_rows_by!(["Date"])
 
-    @table = Grouping(table,:by => "Activity name")
-    respond_with_formatter @table, TestController, "Hour report for #{user.fullname}"
+    activities_data = @activities_summary.collect { |a| [a[:name],a[:hours]] }
+    activities_data << ['Sum',@activities_summary.collect { |i| i[:hours] }.sum]
+
+#    summary =  Ruport::Data::Group.new( :name => 'Summary',:data => activities_data,
+#      :column_names => ['Activity name','Hours'] )
+
+    @table = Grouping(table,:by => "Activity name", :order => :name)
+
+    @summary = @table.summary(:name, :hours => lambda { |g| g.sigma(:Hours) },
+                     :order => [:name] )
+
+
+    #@table.sort_grouping_by! { |g| (g.name == 'Summary' ? 'h' : 'b') + g.name }
+
+    respond_with_formatter @table, TestController, "Hour report for #{@user.fullname}"
   end
 
   private
@@ -44,9 +58,13 @@ class MonthController < ApplicationController
     @last_in_month = (@day >> 1) -1
     @user = current_user_session.user
     @activities = []
-      @user.time_entries.between(@day,@day >> 1).each { |t|
-        @activities << t.activity unless @activities.include? t.activity
-      }
+    @user.time_entries.between(@day,@last_in_month).each do |t|
+      @activities << t.activity unless @activities.include? t.activity
+    end
+    @activities_summary = @activities.collect do |activity|
+      { :name => activity.name,
+        :hours => activity.time_entries.for_user(@user).between(@day,@last_in_month).sum(:hours) }
+    end
   end
 
 end
