1350179e16826c12d9d6767451278642dd0b8a8a
Håkon Torjus Bommen <hakon.bommen@conduct.no>
2009-10-05 10:42:18 +0200

    Styling og vacation overview (index). Placed activity list in month review in the helper

--- app/helpers/month_reviews_helper.rb
+++ app/helpers/month_reviews_helper.rb
@@ -7,5 +7,28 @@ module MonthReviewsHelper
     html += @period.balance_workdays ? " in the first #{@period.balance_workdays} working days" : ''
     html
   end
+  
+  def activities_list(activities_summary)
+    sum = 0
+    html = ""
+    activities_summary.each do |entry|
+      html += "<tr>"
+      html += "<td>#{entry[:name]}</td>"
+      html += "<td style=\"text-align:right\">#{entry[:hours]}</td>"
+      html += "</tr>"
+      sum += entry[:hours]
+    end
+
+    if sum > 0
+      html += "<tr>
+      <th class=\"overline\">
+        Total:
+      </th>
+      <th class=\"overline\" style=\"text-align:right; min-width:3em;\">
+        #{sum}
+      </th>
+    </tr>"
+    end
+  end
 
 end
