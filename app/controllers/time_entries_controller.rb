91e0b109294bcd6549f051086efd886baf50ac1e
Peter Skeide <peterskeide@gmail.com>
2009-10-11 17:50:00 +0200

    Reenabled tags for time entries. Made javascript a requirement for time entries.

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -22,7 +22,6 @@ class TimeEntriesController < ApplicationController
     @time_entry = TimeEntry.new(:date => date)
     @activities = @user.current_activities(date)
     respond_to do |format|
-      format.html {}
       format.js { render :template => "/time_entries/time_entries_with_form.rjs" }
     end
   end
@@ -34,19 +33,11 @@ class TimeEntriesController < ApplicationController
     end
     if @time_entry.save
       respond_to do |format|
-        format.html {
-          flash[:notice] = "Time Entry saved"
-          redirect_to user_time_entries_url(@user, :date => @time_entry.date.beginning_of_week)
-        }
         format.js { render :template => "/time_entries/time_entries.rjs" }
       end
     else
       @activities = @user.current_activities(@time_entry.date)
       respond_to do |format|
-        format.html {
-          flash[:error] = "Unable to create time entry"
-          render :action => "new"
-        }
         format.js { render :template => "/time_entries/time_entries_with_form.rjs" }
       end           
     end 
@@ -56,14 +47,12 @@ class TimeEntriesController < ApplicationController
     @time_entry = @user.time_entries.find(params[:id])
     @activities = @user.current_activities(@time_entry.date)
     respond_to do |format|
-      format.html { }
       format.js { render :template => "/time_entries/time_entries_with_form.rjs" }
     end
   end
     
   def update
     @time_entry = @user.time_entries.find(params[:id])
-
     if params[:tags].respond_to? :each
       tags = []
       params[:tags].each { |id, value| tags << Tag.find(id.to_i) if value == 'true'}
@@ -71,19 +60,11 @@ class TimeEntriesController < ApplicationController
     end
     if @time_entry.update_attributes(params[:time_entry])
       respond_to do |format|
-        format.html {
-          flash[:notice] = "Time Entry updated"
-          redirect_to user_time_entries_url(@user, :date => @time_entry.date.beginning_of_week)
-        }
         format.js { render :template => "/time_entries/time_entries.rjs" }
       end     
     else
       @activities = @user.current_activities(@time_entry.date)
       respond_to do |format|
-        format.html {
-          flash[:error] = "Unable to update time entry"
-          render :action => "edit"          
-        }
         format.js { render :template => "/time_entries/time_entries_with_form.rjs" }
       end
     end
@@ -93,18 +74,9 @@ class TimeEntriesController < ApplicationController
     @time_entry = @user.time_entries.find(params[:id])
     @time_entry.destroy
     respond_to do |format|
-      format.html {
-        flash[:notice] = "Time Entry deleted"
-        redirect_to user_time_entries_url(@user, :date => @time_entry.date.beginning_of_week)
-      }
       format.js { render :template => "/time_entries/time_entries.rjs" }
     end
   end 
-  
-  def confirm_destroy
-    @date = Date.parse(params[:date])
-    @time_entry = @user.time_entries.find(params[:id])
-  end
   
   def lock
     @start = Date.parse(params[:start_date])
