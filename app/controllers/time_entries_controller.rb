50e7176399a9712a2f6abfb3981813137c0aba6b
Peter Skeide <peterskeide@gmail.com>
2010-06-17 14:36:00 +0200

    Moved refreshing of day and week totals (in time_entries/index) to a separate action in the TimeEntriesController.

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -2,7 +2,7 @@ class TimeEntriesController < ApplicationController
     
   before_filter :check_authentication
   before_filter :check_parent_user, :except => :change_user
-  before_filter :find_user, :except => :add_tag
+  before_filter :find_user
          
   def index 
     @date = params[:date].blank? ? Date.today.beginning_of_week : Date.parse(params[:date]).beginning_of_week
@@ -30,14 +30,8 @@ class TimeEntriesController < ApplicationController
   # TODO: implement support for tags 
   def create
     @time_entry = @user.time_entries.build(params[:time_entry])
-    #if params[:tags].respond_to? :each
-    #  params[:tags].each { |id, value| @time_entry.tags << Tag.find(id.to_i) if value == 'true'}
-    #end
     if @time_entry.save
-      @date = @time_entry.date
-      calculate_day_and_week_totals
-      @day_name = Date::DAYNAMES[@date.wday]
-      @new_time_entry = TimeEntry.new(:date => @date, :activity_id => @time_entry.activity.id, :hour_type_id => HourType.default.id)
+      @new_time_entry = TimeEntry.new(:date => @time_entry.date, :activity_id => @time_entry.activity.id, :hour_type_id => HourType.default.id)
     else
       @errors = @time_entry.errors.full_messages.join(', ')         
     end
@@ -56,27 +50,27 @@ class TimeEntriesController < ApplicationController
   # TODO: add support for tags  
   def update
     @time_entry = @user.time_entries.find(params[:id])
-    #if params[:tags].respond_to? :each
-    #  tags = []
-    #  params[:tags].each { |id, value| tags << Tag.find(id.to_i) if value == 'true'}
-    #  @time_entry.tags = tags
-    #end
     if @time_entry.update_attributes(params[:time_entry])
-      @date = @time_entry.date
-      calculate_day_and_week_totals
-      @day_name = Date::DAYNAMES[@date.wday]
-      @new_time_entry = TimeEntry.new(:date => @date, :activity_id => @time_entry.activity.id, :hour_type_id => HourType.default.id)
+      @new_time_entry = TimeEntry.new(:date => @time_entry.date, :activity_id => @time_entry.activity.id, :hour_type_id => HourType.default.id)
     else
       @errors = @time_entry.errors.full_messages.join(', ') 
     end
   end
   
+  def refresh_totals
+    @date = Date.parse(params[:date])
+    @day_name = Date::DAYNAMES[@date.wday]
+    @activity = Activity.find(params[:activity_id])
+    time_entries = @user.time_entries.for_activity(@activity).between(@date.beginning_of_week, @date.end_of_week)
+    time_entry_array = MonthReview::TimeEntryArray.new(time_entries)
+    @day_sum = time_entry_array.for_date(@date).sum_hours
+    @week_sum = time_entry_array.sum_hours
+  end
+  
   def destroy
     @time_entry = @user.time_entries.find(params[:id])
     @time_entry.destroy
-    @date = @time_entry.date
-    calculate_day_and_week_totals
-    @day_name = Date::DAYNAMES[@date.wday]
+    @no_more_time_entries = @user.time_entries.on_day(@date).for_activity(@time_entry.activity).empty? # Optimize this!
   end 
   
   def lock
@@ -87,14 +81,7 @@ class TimeEntriesController < ApplicationController
   end
 
   private
-  
-  def calculate_day_and_week_totals
-    time_entries = @user.time_entries.for_activity(@time_entry.activity).between(@date.beginning_of_week, @date.end_of_week)
-    time_entry_array = MonthReview::TimeEntryArray.new(time_entries)
-    @day_sum = time_entry_array.for_date(@date).sum_hours
-    @week_sum = time_entry_array.sum_hours
-  end
-  
+
   def find_user
     @user = User.find(params[:user_id])
   end
