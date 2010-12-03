8b21f6aff9d4475a478e50a96b67abe765b77729
Håkon Torjus Bommen <hakonb@conduct.no>
2010-12-03 10:14:18 +0100

    Fjernet mulighet for komma desimal separator på grunn av problemer med float verdier på console siden

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -17,9 +17,9 @@ class TimeEntry < ActiveRecord::Base
 
   before_save :validate_changes_on_locked_entry
 
-  def hours=(hours)
-    write_attribute(:hours, hours.gsub(/\,(\d+)$/, '.\1'))
-  end
+  #def hours=(hours)
+  #  write_attribute(:hours, hours.gsub(/\,(\d+)$/, '.\1'))
+  #end
   
   def before_destroy
     if locked
