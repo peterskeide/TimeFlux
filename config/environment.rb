e05b60311a181942f8f73ecc27a11f0dd175b312
Håkon Torjus Bommen <hakonb@conduct.no>
2011-11-10 09:54:03 +0100

    Added bundler for gem dependency ref: http://devcenter.heroku.com/articles/bundler and http://gembundler.com/rails23.html

--- config/environment.rb
+++ config/environment.rb
@@ -27,14 +27,16 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "authlogic", :version => '2.1.2'
-  #config.gem "ruport"
-  config.gem 'mislav-will_paginate', :version => '2.3.11', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  config.gem 'ruby-net-ldap', :lib => 'net/ldap'
-  config.gem "prawn", :version => "0.11.1"
-  config.gem 'prawn-core',      :version => '0.5.1', :lib => 'prawn/core'
-  config.gem 'prawn-format',    :version => '0.2.1', :lib => 'prawn/format'
-  config.gem 'prawn-layout',    :version => '0.2.1', :lib => 'prawn/layout'
+
+  ### Replaced by bundler
+
+  ### config.gem "authlogic", :version => '2.1.2'
+  ### config.gem 'mislav-will_paginate', :version => '2.3.11', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  ### config.gem 'ruby-net-ldap', :lib => 'net/ldap'
+  ### config.gem "prawn", :version => "0.11.1"
+  ### config.gem 'prawn-core',      :version => '0.5.1', :lib => 'prawn/core'
+  ### config.gem 'prawn-format',    :version => '0.2.1', :lib => 'prawn/format'
+  ### config.gem 'prawn-layout',    :version => '0.2.1', :lib => 'prawn/layout'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
